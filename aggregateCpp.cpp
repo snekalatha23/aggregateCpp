0b411df92f14d9664e3aeb2106e6acf392364992
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2020-01-01 14:05:37 +0530

    Create aggregateCpp.cpp

---
+++ aggregateCpp.cpp
@@ -0,0 +1,42 @@
+# include<iostream.h>
+class Marks
+{
+int rno;
+
+int sub1,sub2;
+
+public:
+
+void readmarks()
+
+{
+
+cout<<"\n Input Two subjects";
+
+cin>>sub1>>sub2;
+
+}
+
+void compare(Marks s)
+
+{
+
+if (sub1 ==s,sub1&& sub2==s.sub2)
+
+cout<< "\n Marks are not equal";
+
+}
+
+}
+
+void Main()
+
+{
+
+Marks stud1,stud2;
+
+stud2.read-marks();
+
+stud1.compare(stud2);
+
+}
